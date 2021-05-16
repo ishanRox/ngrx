f49668ef68dc3952b28f7b1b057a68936c68a672
ishanRox <roxishan8@gmail.com>
2021-05-16 18:19:17 +0530

    custome increment ui added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,12 @@ import { C2Component } from './c2/c2.component';
 //ngrx store
 import {StoreModule} from '@ngrx/store';
 import { counterReducer } from './state/counter.reducer';
+import { C3Component } from './c3/c3.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,
   StoreModule.forRoot({ishanCounter:counterReducer}) ],
-  declarations: [ AppComponent, HelloComponent, C1Component, C2Component ],
+  declarations: [ AppComponent, HelloComponent, C1Component, C2Component, C3Component ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/c3/c3.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-c3',
+  templateUrl: './c3.component.html',
+  styleUrls: ['./c3.component.css']
+})
+export class C3Component implements OnInit {
+  customeVal: number;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onCusIncrement() {
+    console.log(this.customeVal);
+  }
+}
