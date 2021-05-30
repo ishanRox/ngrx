b3ac892fc72c5a7ce2c5d77995fa06c4cc51064a
ishanRox <roxishan8@gmail.com>
2021-05-30 23:40:26 +0530

    post list added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,11 +15,12 @@ import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { HeaderComponent } from './shared/header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,
   StoreModule.forRoot({ishanCounter:counterReducer}) ,AppRoutingModule ],
-  declarations: [ AppComponent, HelloComponent, C1Component, C2Component, C3Component, HomeComponent, CounterComponent, HeaderComponent ],
+  declarations: [ AppComponent, HelloComponent, C1Component, C2Component, C3Component, HomeComponent, CounterComponent, HeaderComponent, PostListComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
