6f93280d00e7b41aa9d505f8642f6b60e094343c
ishanRox <roxishan8@gmail.com>
2021-05-31 10:11:33 +0530

    added ngrx dev tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,9 +17,15 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
 import { HeaderComponent } from './shared/header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 
+
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'; 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,
-  StoreModule.forRoot({ishanCounter:counterReducer}) ,AppRoutingModule ],
+  StoreModule.forRoot({ishanCounter:counterReducer}) ,
+    StoreDevtoolsModule.instrument({
+    
+    }),
+  AppRoutingModule ],
   declarations: [ AppComponent, HelloComponent, C1Component, C2Component, C3Component, HomeComponent, CounterComponent, HeaderComponent, PostListComponent ],
   bootstrap:    [ AppComponent ]
 })
