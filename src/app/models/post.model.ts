264f919e9a8aada94d91f190beb6f2e3081dae14
ishanRox <roxishan8@gmail.com>
2021-06-02 20:34:50 +0530

    added state for posts

---
+++ src/app/models/post.model.ts
@@ -0,0 +1,7 @@
+import { Title } from "@angular/platform-browser";
+
+export interface Post {
+  id:string;
+  title:string;
+  description:string;
+  }
---
+++ src/app/posts/state/posts.state.ts
@@ -0,0 +1,22 @@
+import { Post } from '../../models/post.model';
+
+export interface PostState {
+  posts: Post[];
+}
+
+export const initialState: PostState = {
+  posts: [
+    {
+      id: '1',
+      title: 'sample title 1',
+      description: 'Sample description 1'
+    }
+    ,
+      {
+      id: '2',
+      title: 'sample title 2',
+      description: 'Sample description 2'
+    }
+ 
+  ]
+};
