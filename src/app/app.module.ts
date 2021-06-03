830db7ed85ca166323b1f79b4d15e8c0fa176ba7
ishanRox <roxishan8@gmail.com>
2021-06-03 09:16:58 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { C1Component } from './c1/c1.component';
 import { C2Component } from './c2/c2.component';
 
 //ngrx store
-import {StoreModule} from '@ngrx/store';
+import { StoreModule } from '@ngrx/store';
 import { counterReducer } from './state/counter.reducer';
 import { C3Component } from './c3/c3.component';
 import { HomeComponent } from './home/home.component';
@@ -17,16 +17,27 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
 import { HeaderComponent } from './shared/header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 
-
-import { StoreDevtoolsModule } from '@ngrx/store-devtools'; 
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { appReducer } from './store/app.state';
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,
-  StoreModule.forRoot({ishanCounter:counterReducer}) ,
-    StoreDevtoolsModule.instrument({
-    
-    }),
-  AppRoutingModule ],
-  declarations: [ AppComponent, HelloComponent, C1Component, C2Component, C3Component, HomeComponent, CounterComponent, HeaderComponent, PostListComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    StoreModule.forRoot(appReducer),
+    StoreDevtoolsModule.instrument({}),
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    C1Component,
+    C2Component,
+    C3Component,
+    HomeComponent,
+    CounterComponent,
+    HeaderComponent,
+    PostListComponent
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
